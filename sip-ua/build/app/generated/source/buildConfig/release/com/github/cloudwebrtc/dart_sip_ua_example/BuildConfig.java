8f5a7504101f7a891353233f31fd50ff44fc0cfc
ouledelbakri <yoebakri017@enseirb-matmeca.fr>
2024-11-22 22:51:52 +0100

    Setup for SIP communication with Dart and Flutter.

---
+++ sip-ua/build/app/generated/source/buildConfig/release/com/github/cloudwebrtc/dart_sip_ua_example/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.github.cloudwebrtc.dart_sip_ua_example;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.github.cloudwebrtc.dart_sip_ua_example";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
